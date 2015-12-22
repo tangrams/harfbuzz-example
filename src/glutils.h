d13097ccec7095433b00c4fabdac21921aedf815
Karim Naaji <karim.naaji@gmail.com>
2015-12-22 17:14:32 -0500

    Add glew

--- src/glutils.h
+++ src/glutils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "GL/glew.h"
 #include "GLFW/glfw3.h"
 #include <vector>
 
@@ -121,6 +122,10 @@ namespace gl {
         }
 
         glfwMakeContextCurrent(g_window);
+
+        glewExperimental = GL_TRUE;
+
+        assert(glewInit() == GLEW_OK);
 
         glfwSetWindowSizeCallback(g_window, resize);
 
